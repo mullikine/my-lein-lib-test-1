f32b4e3c0efb50c9aa1b3042ac95150c6ba764ae
Shane Mulligan <mullikine@gmail.com>
2020-08-14 00:26:21 +1200

    14.08.20

---
+++ myleinlibrary/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to myleinlibrary
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ myleinlibrary/test/myleinlibrary/core_test.clj
@@ -0,0 +1,7 @@
+(ns myleinlibrary.core-test
+  (:require [clojure.test :refer :all]
+            [myleinlibrary.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
